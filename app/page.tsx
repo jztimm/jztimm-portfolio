11bf79a42079cbaeb0586dedb02a3ca1962e319d
jztimm <jztimm@gmail.com>
2024-07-01 22:42:57 -0400

    Created and Updated Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative flex bg-black-100 justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>Hello, Portfolio!</h1>
+        <Hero />
       </div>
     </main>
   );
