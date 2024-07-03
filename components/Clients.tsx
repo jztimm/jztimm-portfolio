44d4e74cf64b15bcee7530bb97076f1a6b36ce97
jztimm <jztimm@gmail.com>
2024-07-02 22:39:05 -0400

    Update Sentry options and created Clients component

---
+++ components/Clients.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Clients = () => {
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        Kind words from <span className="text-purple"> satisfied clients</span>
+      </h1>
+
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10"></div>
+    </div>
+  );
+};
+
+export default Clients;
