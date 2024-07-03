71b657cc4211508bf08a136f6710d67480a6ed9b
jztimm <jztimm@gmail.com>
2024-07-02 23:22:29 -0400

    Completed Experience section

--- data/index.ts
+++ data/index.ts
@@ -196,29 +196,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Frontend Engineer - FTC/TRIA",
+    desc: "Enhanced application features, improving operational efficiency by reducing load times by 10%. Facilitated communication between managers and clients, integrating user feedback.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
+    title: "Software Engineer - IBM",
+    desc: "Translated client requirements into dynamic features using AngularJS and ReactJS. Spearheaded CI/CD adoption with Jenkins and Travis CI, boosting software scalability and performance.",
+    className: "md:col-span-2",
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
+    title: "Full Stack Developer - Freedom Stairlifts",
+    desc: "Implemented responsive UIs with React.js and Next.js. Utilized Tailwind CSS for senior-friendly designs and architected Microservices for improved scalability and performance.",
+    className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    desc: "Developed and maintained user-facing features using modern frontend technologies. Led the adoption of best practices, ensuring high-quality and scalable applications.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
